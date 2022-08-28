3c06cde42ccf016bd22389b10645c25ac9e4d29f
mustafa demirtaş <mustafademirtas94@gmail.com>
2022-08-28 22:41:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKey,TValue> 
+    {
+        KeyValuePair<TKey, TValue>[] item;
+        
+
+        public MyDictionary()
+        {
+            item = new KeyValuePair<TKey, TValue>[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            KeyValuePair<TKey?, TValue>[] tmp = item;
+            item = new KeyValuePair<TKey, TValue>[tmp.Length+1];
+            for (int i =0;i<tmp.Length;i++)
+            {
+                item[i] = tmp[i];
+            }
+            item[tmp.Length] =KeyValuePair.Create(key, value);
+         
+        }
+        
+      
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+MyDictionary<string, string> map = new();
+map.Add("a", "A");
+map.Add("b", "B");
+
+
